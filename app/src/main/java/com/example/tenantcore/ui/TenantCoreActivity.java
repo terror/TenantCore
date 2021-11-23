a315de71cf7ffefc5813f5076e5fce787f935ca2
Liam <liam@scalzulli.com>
2021-11-23 10:28:00 -0500

    add landlord home fragment and request list

--- app/src/main/java/com/example/tenantcore/ui/TenantCoreActivity.java
+++ app/src/main/java/com/example/tenantcore/ui/TenantCoreActivity.java
@@ -3,7 +3,6 @@ package com.example.tenantcore.ui;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -11,7 +10,6 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import com.example.tenantcore.R;
 import com.example.tenantcore.databinding.ActivityTenantcoreBinding;
-import com.google.android.material.snackbar.Snackbar;
 
 public class TenantCoreActivity extends AppCompatActivity {
   private AppBarConfiguration appBarConfiguration;
@@ -29,14 +27,6 @@ public class TenantCoreActivity extends AppCompatActivity {
     NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
     appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
     NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-    binding.fab.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-          .setAction("Action", null).show();
-      }
-    });
   }
 
   @Override
@@ -48,15 +38,10 @@ public class TenantCoreActivity extends AppCompatActivity {
 
   @Override
   public boolean onOptionsItemSelected(MenuItem item) {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
     int id = item.getItemId();
 
-    //noinspection SimplifiableIfStatement
-    if (id == R.id.action_settings) {
+    if (id == R.id.action_settings)
       return true;
-    }
 
     return super.onOptionsItemSelected(item);
   }
