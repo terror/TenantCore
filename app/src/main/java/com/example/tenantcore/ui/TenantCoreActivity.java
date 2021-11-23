7c5791cc2c51d9072b160bba266c4fdc31bb1a23
Xermax <Xermax3@users.noreply.github.com>
2021-11-23 09:24:14 -0500

    Folder structure & Home navigation

--- app/src/main/java/com/example/tenantcore/ui/TenantCoreActivity.java
+++ app/src/main/java/com/example/tenantcore/ui/TenantCoreActivity.java
@@ -1,4 +1,4 @@
-package com.example.tenantcore;
+package com.example.tenantcore.ui;
 
 import android.os.Bundle;
 import android.view.Menu;
@@ -9,18 +9,20 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import com.example.tenantcore.databinding.ActivityMainBinding;
+
+import com.example.tenantcore.R;
+import com.example.tenantcore.databinding.ActivityTenantCoreBinding;
 import com.google.android.material.snackbar.Snackbar;
 
-public class MainActivity extends AppCompatActivity {
+public class TenantCoreActivity extends AppCompatActivity {
   private AppBarConfiguration appBarConfiguration;
-  private ActivityMainBinding binding;
+  private ActivityTenantCoreBinding binding;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
-    binding = ActivityMainBinding.inflate(getLayoutInflater());
+    binding = ActivityTenantCoreBinding.inflate(getLayoutInflater());
     setContentView(binding.getRoot());
 
     setSupportActionBar(binding.toolbar);
@@ -28,14 +30,6 @@ public class MainActivity extends AppCompatActivity {
     NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
     appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
     NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-    binding.fab.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-          .setAction("Action", null).show();
-      }
-    });
   }
 
   @Override
