f93d4f78f64356a9b312349ad15eca7a45e0021f
Ishpal Mattu <IshpalSMattu@hotmail.com>
2021-11-24 15:41:57 -0500

    Added Models + given ids to components
    Models added with basic fields:
    - Priority --> Enum indicating priority of request
    - Status --> Enum indicating status of request
    - Request --> Indicates a request that a Tenant makes
    - Tenant --> Represents a tenant that makes requests to landlord

---
+++ app/src/main/java/com/example/tenantcore/model/Priority.java
@@ -0,0 +1,7 @@
+package com.example.tenantcore.model;
+
+public enum Priority {
+  LOW,
+  MEDIUM,
+  HIGH,
+}
--- app/src/main/java/com/example/tenantcore/model/Request.java
+++ app/src/main/java/com/example/tenantcore/model/Request.java
@@ -1,4 +1,64 @@
 package com.example.tenantcore.model;
 
+import java.util.Date;
+
 public class Request {
+
+  private String title;
+  private String description;
+  private Date dueDate;
+  private Status status;
+  private Priority priority;
+
+  public Request(String title, String description, Date dueDate){
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+  }
+
+  public Status getStatus() {
+    return status;
+  }
+
+  public void setStatus(Status status) {
+    this.status = status;
+  }
+
+  public Priority getPriority() {
+    return priority;
+  }
+
+  public void setPriority(Priority priority) {
+    this.priority = priority;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public Date getDueDate() {
+    return dueDate;
+  }
+
+  public void setDueDate(Date dueDate) {
+    this.dueDate = dueDate;
+  }
+
+
+
+
+
+
 }
